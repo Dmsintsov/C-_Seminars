3291446a7415980e058bac4a01939ffa38e38249
Dmitry Sintsov <dmsincov@yandex.ru>
2023-04-10 19:58:12 +0200

    Добавили решения задач 2,4,5,6.7.8

--- Task03/Program.cs
+++ Task03/Program.cs
@@ -3,4 +3,14 @@
 // 3 -> Среда
 // 5 -> Пятница
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Напишите целое число от 1 до 7: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number == 1) Console.WriteLine("Понедельник");
+if (number == 2) Console.WriteLine("Вторник");
+if (number == 3) Console.WriteLine("Среда");
+if (number == 4) Console.WriteLine("Четверг");
+if (number == 5) Console.WriteLine("Пятница");
+if (number == 6) Console.WriteLine("Суббота");
+if (number == 7) Console.WriteLine("Восресенье");
+if (number > 7) Console.WriteLine("Нет такого дня недели.");
+if (number < 1) Console.WriteLine("Нет такого дня недели.");
