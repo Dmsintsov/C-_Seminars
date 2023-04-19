1cfc37d41de99b51916bd80d50c9690564e6bd3c
Dmitry Sintsov <dmsincov@yandex.ru>
2023-04-19 20:45:02 +0200

    задачи 24 и 26

---
+++ Task24/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 24: Напишите программу, которая
+// принимает на вход число (А) и выдаёт сумму чисел
+// от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+int Text(string message)
+{
+    Console.WriteLine(message);
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+int number = Text ("Введите число: ");
+int Sum (int num)
+{
+int res = 0;
+for (int i = 0; i < num; i++)
+{
+    res += i+1;
+
+
+}
+return res;
+
+}
+int result = Sum (number);
+System.Console.WriteLine(result);
---
+++ Task26/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 26: Напишите программу, которая принимает
+// на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+int Text(string message)
+{
+    Console.WriteLine(message);
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+int number = Text("Введите число: ");
+
+int Length(int num)
+{
+    int count = 0;
+    while (num!= 0 )
+    {
+num/= 10;
+count++;
+
+    }
+    
+   return count;
+}
+
+int result = Length (number);
+System.Console.WriteLine(result);
