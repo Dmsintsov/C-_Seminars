a2fefcdde3fe70aea4e14ce45efaaf18b6df8c95
Dmitry Sintsov <dmsincov@yandex.ru>
2023-04-23 21:58:01 +0200

    задачи 32,33

--- Task32/Program.cs
+++ Task32/Program.cs
@@ -2,4 +2,56 @@
 // массива: положительные элементы замените на
 // соответствующие отрицательные, и наоборот.
 // [-4, -8, 8, 2] -> [4, 8, -8, -2]
-Console.WriteLine("Hello, World!");
+int Text(string message)
+{
+    Console.WriteLine(message);
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+int lengthArray = Text("Введите длину массива: ");
+
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    var random = new Random();
+    for (int i = 0; i < length; i++)
+    {
+        collection[i] = random.Next(-9, 10);
+    }
+
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    for (int position = 0; position < count; position++)
+    {
+        if (position < col.Length - 1) Console.Write(col[position] + ", ");
+        else
+        {
+            Console.Write(col[position]);
+        }
+    }
+
+}
+
+
+int[] array = new int[lengthArray];
+FillArray(array);
+Console.Write("[");
+PrintArray(array);
+Console.Write("] -> [");
+
+void Reverse(int[] arr)
+{
+    int length = array.Length;
+for (int j = 0; j < length; j++)
+{
+    arr[j] = -array[j];
+}
+}
+
+Reverse(array);
+PrintArray(array);
+Console.Write("]");
+Console.WriteLine();
--- Task33/Program.cs
+++ Task33/Program.cs
@@ -2,4 +2,70 @@
 // определяет, присутствует ли заданное число в массиве.
 // 4; массив [6, 7, 19, 345, 3] -> нет
 // 3; массив [6, 7, 19, 345, 3] -> да
-Console.WriteLine("Hello, World!");
+
+int Text(string message)
+{
+    Console.WriteLine(message);
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+
+int TextNumber(string mess)
+{
+    Console.WriteLine(mess);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+int lengthArray = Text("Введите длину массива: ");
+int textNumber = TextNumber("Введите число: ");
+
+
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    var random = new Random();
+    for (int i = 0; i < length; i++)
+    {
+        collection[i] = random.Next(-9, 10);
+    }
+
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    for (int position = 0; position < count; position++)
+    {
+        if (position < col.Length - 1) Console.Write(col[position] + ", ");
+        else
+        {
+            Console.Write(col[position]);
+        }
+    }
+
+}
+
+bool PresenseNumber (int[] arr)
+
+{
+int length = arr.Length;
+for (int j = 0; j < length; j++)
+{
+    if (arr[j] == textNumber) return true;
+    }
+return false;
+}
+
+int[] array = new int[lengthArray];
+FillArray(array);
+Console.Write("[");
+PrintArray(array);
+Console.WriteLine("]");
+
+bool presenceNumber = PresenseNumber (array);
+// if (presenceNumber == true) Console.WriteLine("да");
+// else{ Console.WriteLine("нет");
+// }
+
+Console.WriteLine(presenceNumber? "Да": "нет");
