fb9d72f84c5b56a8883810479c25a7ae88d1f133
Dmitry Sintsov <dmsincov@yandex.ru>
2023-04-24 22:59:49 +0200

    задачи 35,37

---
+++ Task35/Program.cs
@@ -0,0 +1,67 @@
+﻿// Задача 35: Задайте одномерный массив из 123 случайных чисел.
+// Найдите количество элементов массива, значения которых лежат в
+// отрезке [10,99].
+// Пример для массива из 5, а не 123 элементов. В своём решении сделайте для
+// 123
+// [5, 18, 123, 6, 2] -> 1
+// [1, 2, 3, 6, 2] -> 0
+// [10, 11, 12, 13, 14] -> 5
+
+
+int Text(string message)
+{
+    Console.WriteLine(message);
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    var random = new Random();
+    for (int i = 0; i < length; i++)
+    {
+        collection[i] = random.Next(0, 1000);
+    }
+
+}
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    for (int position = 0; position < count; position++)
+    {
+        if (position < col.Length - 1) Console.Write(col[position] + ", ");
+        else
+        {
+            Console.Write(col[position]);
+        }
+    }
+
+}
+
+int AmountNumbers (int[] arr)
+{
+int size = arr.Length;
+int count = 0;
+
+for (int j = 0; j < size; j++)
+{
+   if (arr[j]>9 && arr[j]<100)
+   {
+    count +=1;
+   }
+}
+return count;
+    
+}
+
+
+
+int lengthArray = Text("Введите длину массива: ");
+int[] array = new int[lengthArray];
+FillArray(array);
+Console.Write("[");
+PrintArray(array);
+Console.WriteLine("]");
+Console.WriteLine();
+int amountNumbers = AmountNumbers (array);
+Console.WriteLine($"Количество двузначных чисел в массиве: {amountNumbers}");
---
+++ Task37/Program.cs
@@ -0,0 +1,171 @@
+﻿// Задача 37: Найдите произведение пар чисел в одномерном массиве.
+// Парой считаем первый и последний элемент, второй и предпоследний
+// и т.д. Результат запишите в новом массиве.
+// [1 2 3 4 5] -> 5 8 31
+// [6 7 3 6] -> 36 21
+
+// int Text(string message)
+// {
+//     Console.WriteLine(message);
+//     int result = Convert.ToInt32(Console.ReadLine());
+//     return result;
+// }
+// void FillArray(int[] collection)
+// {
+//     int length = collection.Length;
+//     var random = new Random();
+//     for (int i = 0; i < length; i++)
+//     {
+//         collection[i] = random.Next(0, 1000);
+//     }
+
+// }
+// void PrintArray(int[] col)
+// {
+//     int count = col.Length;
+//     for (int position = 0; position < count; position++)
+//     {
+//         if (position < col.Length - 1) Console.Write(col[position] + ", ");
+//         else
+//         {
+//             Console.Write(col[position]);
+//         }
+//     }
+
+// }
+
+// void SumPairs(int[] arr, int[] arr2)
+// {
+//     int length = arr.Length;
+//     int k = 0;
+
+//     if (length % 2 == 0)
+//     {
+//         for (int j = 0; j < length / 2; j++)
+//         {
+//             arr2[k] = arr[j] + arr[length - 1 - k];
+//             k += 1;
+//         }
+//     }
+//     else
+//     {
+
+//         for (int j = 0; j < length / 2; j++)
+//         {
+//             arr2[k] = arr[j] + arr[length - 1 - k];
+//             k += 1;
+
+//         }
+
+//         arr2[k] = arr[length / 2];
+//     }
+
+// }
+
+
+
+
+// int lengthArray = Text("Введите длину массива: ");
+// int[] array = new int[lengthArray];
+// FillArray(array);
+// Console.Write("[");
+// PrintArray(array);
+// Console.WriteLine("]");
+// Console.WriteLine();
+
+// int newLength = lengthArray / 2;
+// if (lengthArray % 2 != 0)    newLength = lengthArray / 2 + 1;
+
+
+// int[] newArr1 = new int[newLength];
+// SumPairs(array, newArr1);
+// Console.Write("[");
+// PrintArray(newArr1);
+// Console.WriteLine("]");
+// Console.WriteLine();
+
+
+// Задача 37: Найдите произведение пар чисел в одномерном массиве.
+// Парой считаем первый и последний элемент, второй и предпоследний
+// и т.д. Результат запишите в новом массиве.
+// [1 2 3 4 5] -> 5 8 31
+// [6 7 3 6] -> 36 21
+
+int Text(string message)
+{
+    Console.WriteLine(message);
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    var random = new Random();
+    for (int i = 0; i < length; i++)
+    {
+        collection[i] = random.Next(0, 10);
+    }
+
+}
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    for (int position = 0; position < count; position++)
+    {
+        if (position < col.Length - 1) Console.Write(col[position] + ", ");
+        else
+        {
+            Console.Write(col[position]);
+        }
+    }
+
+}
+
+int[] ProductPairs(int[] arr)
+{
+    int length = arr.Length;
+    int k = 0;
+    int newLength = arr.Length / 2;
+    if (arr.Length % 2 != 0) newLength = arr.Length / 2 + 1;
+    int[] newArr1 = new int[newLength];
+
+    if (length % 2 == 0)
+    {
+        for (int j = 0; j < length / 2; j++)
+        {
+            newArr1[k] = arr[j] * arr[length - 1 - k];
+            k += 1;
+        }
+    }
+    else
+    {
+
+        for (int j = 0; j < length / 2; j++)
+        {
+            newArr1[k] = arr[j] * arr[length - 1 - k];
+            k += 1;
+
+        }
+
+        newArr1[k] = arr[length / 2];
+    }
+return newArr1;
+}
+
+
+
+
+int lengthArray = Text("Введите длину массива: ");
+int[] array = new int[lengthArray];
+FillArray(array);
+Console.Write("[");
+PrintArray(array);
+Console.WriteLine("]");
+Console.WriteLine();
+
+
+int[] result = ProductPairs(array);
+Console.Write("[");
+PrintArray(result);
+Console.WriteLine("]");
+Console.WriteLine();
