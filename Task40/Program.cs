8dc825eca0d58767b85e97fea96002495c6eb126
Dmitry Sintsov <dmsincov@yandex.ru>
2023-05-07 21:54:32 +0200

    задачи 40,42

--- Task40/Program.cs
+++ Task40/Program.cs
@@ -3,3 +3,30 @@
 // такой длины.
 // Теорема о неравенстве треугольника: каждая сторона треугольника
 // меньше суммы двух других сторон.
+
+
+int Text(string message)
+{
+    Console.WriteLine(message);
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+
+bool Triangle (int numA, int numB, int numC)
+{
+if (numA < numB + numC && numB < numA + numC && numC < numB + numA) return true;
+else return false;
+}
+
+
+
+
+int numberA = Text ("Введите положительное число A: ");
+int numberB = Text ("Введите положительное число B: ");
+int numberC = Text ("Введите положительное число C: ");
+
+bool result = Triangle (numberA, numberB, numberC); 
+if (numberA <=0 || numberB <=0 || numberC <=0) Console.WriteLine("Введены неверные числа");
+else
+{Console.WriteLine(result? "Треугольник может существовать со сторонами такой длины": "Треугольник не может существовать со сторонами такой длины");
+}
--- Task42/Program.cs
+++ Task42/Program.cs
@@ -3,3 +3,27 @@
 // 45 -> 101101
 // 3 -> 11
 // 2 -> 10
+
+int Text(string message)
+{
+    Console.WriteLine(message);
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+
+double BinaryNumber (int num)
+{
+double bin = 0;
+for (int i = 0; num >0; i++)
+{
+    bin = bin + num %2 * Math.Pow (10, i);
+    num = num / 2;
+}
+return bin;
+
+}
+
+int number = Text ("Введите положительное число: ");
+double res = BinaryNumber (number);
+Console.WriteLine(number<0? "Введено неверное число": $"{res}");
+
