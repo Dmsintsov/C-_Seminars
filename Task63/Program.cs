e1325f6c44fe24f2fe16e04059e4d609d313e282
Dmitry Sintsov <dmsincov@yandex.ru>
2023-05-30 21:53:28 +0200

    63, 65

---
+++ Task63/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 63: Задайте значение N. Напишите программу, которая выведет
+// все натуральные числа в промежутке от 1 до N.
+// N = 5 -> "1, 2, 3, 4, 5"
+// N = 6 -> "1, 2, 3, 4, 5, 6"
+
+int Text(string message)
+{
+    Console.WriteLine(message);
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+void NaturalNumbers(int num)
+{
+    if (num == 0) return;
+    NaturalNumbers(num - 1);
+    Console.Write($"{num} ");
+}
+
+int length = Text("Введите целое положительное число: ");
+NaturalNumbers(length);
---
+++ Task65/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 65: Задайте значения M и N. Напишите программу, которая
+// выведет все натуральные числа в промежутке от M до N.
+// M = 1; N = 5 -> "1, 2, 3, 4, 5"
+// M = 4; N = 8 -> "4, 5, 6, 7, 8"
+
+int Text(string message)
+{
+    Console.WriteLine(message);
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+void NaturalNumbersMinToMax(int numA, int numB)
+{
+    if (numA - 1 == numB) return;
+    else
+    {
+
+        NaturalNumbersMinToMax(numA, numB - 1);
+        Console.Write($"{numB} ");
+    }
+}
+
+void NaturalNumbersMaxToMin(int numA, int numB)
+{
+    if (numA +1 == numB) return;
+    else
+    {
+
+        NaturalNumbersMaxToMin(numA, numB + 1);
+        Console.Write($"{numB} ");
+    }
+}
+
+int numberA = Text("Введите целое положительное число: ");
+int numberB = Text("Введите целое положительное число: ");
+if (numberA < numberB)
+    NaturalNumbersMinToMax(numberA, numberB);
+else {NaturalNumbersMaxToMin(numberA, numberB);}
